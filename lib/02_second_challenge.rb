9a18d347096cc60d1b1958dc1e632db561808414
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-09 20:32:48 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,4 +1,19 @@
 def second_challenge
-  epic_tragedy = {:montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []}, 
-    :capulet => {:partriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []}}
+  epic_tragedy = {
+    :montague => {
+     :patriarch => {}, 
+     :matriarch => {}, 
+     :hero => {}, 
+     :hero_friends => []
+      
+    }, 
+    :capulet => {
+      :patriarch => {}, 
+      :matriarch => {}, 
+      :heroine => {}, 
+      :heroine_friends => []
+      
+    }
+    
+  }
 end
